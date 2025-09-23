9a00ac8ae2b5dad988b8bac1d8facdcda9f516d7
Lucas Dummett <lucas.dummett@my.jcu.edu.au>
2025-09-23 13:18:27 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,19 @@
 MINIMUM_LENGTH = 10
-password = input("Set password: ")
-while len(password) < 10:
-    password = input("Minimum 10 Characters\nSet password: ")
-print('*' * len(password))
+
+def main():
+    password = get_password()
+    print_password_asterisk(password)
+
+
+def print_password_asterisk(password):
+    print('*' * len(password))
+
+
+def get_password():
+    password = input("Set password: ")
+    while len(password) < 10:
+        password = input("Minimum 10 Characters\nSet password: ")
+    return password
+
+
+main()
