17b2680b228886810a39b327c89ecd9612616ea7
lucas dummett <lucas.dummett@my.jcu.edu.au>
2025-09-30 13:07:36 +1000

    Add question answers and random program

---
+++ prac_03/randoms.py
@@ -0,0 +1,20 @@
+from random import randrange
+
+"""
+Question Answers:
+print(random.randint(5, 20))  # line 1
+- 8
+5, 20
+
+print(random.randrange(3, 10, 2))  # line 2
+- 3
+3, 9
+
+print(random.uniform(2.5, 5.5))  # line 3
+- 4.894510991125033
+2.5, 5.5
+"""
+
+
+
+print(randrange(1,100))
