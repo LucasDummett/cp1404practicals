588c9b867c7653d797fc2912006d90eb5b0587c0
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-05 15:26:48 +1000

    Update answers

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -9,12 +9,11 @@ print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 - 3
 3, 9
+No it cannot
 
 print(random.uniform(2.5, 5.5))  # line 3
 - 4.894510991125033
 2.5, 5.5
 """
 
-
-
-print(randint(1,100))
+print(randint(1, 100))
