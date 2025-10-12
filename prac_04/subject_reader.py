70289c1bcbaae0f97b0b8b047101ca5f26ef497a
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-12 16:25:48 +1000

    Reconfigure subject_reader program to use functions and nested lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,28 +7,29 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    # print(data)
+    """ Read subject information and print in an organised format."""
+    subject_details = load_subject_details(FILENAME)
+    print_subject_details(subject_details)
 
 
-def load_data(filename=FILENAME):
+def load_subject_details(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject_details = []  # Named subject details as it will be a nested list.
     input_file = open(filename)
-    subject_details = []
     for line in input_file:
-        # print(line)  # See what a line looks like
-        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         subject_details.append(parts)
-        # print(parts)  # See if that worked
-        # print("----------")
     input_file.close()
     return subject_details
 
+
 def print_subject_details(subject_details):
+    """Print subject details in an organised format."""
+    for subject_detail in subject_details:
+        print(f"{subject_detail[0]} is taught by {subject_detail[1]:12} and has {subject_detail[2]:3} students")
 
 
 main()
+
