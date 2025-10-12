825d4057b41e82fa7903dbffc62e1de304c91780
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-12 20:29:34 +1000

    Add sanity checks to lists_warmup program

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -26,12 +26,11 @@ numbers + [6, 5, 3] --> Error Answer: 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
 
 # 1
 numbers[0] = "ten"
-
+print(numbers)
 # 2
 numbers[-1] = 1
-
+print(numbers)
 # 3
 print(numbers[2:])
-
 # 4
 print(9 in numbers)
