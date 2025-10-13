58ccae8dd4482d2b3bbec2fb31ed5a007687bf78
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-14 09:50:23 +1000

    Add doc string to hex_colours program in prac 5

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,7 @@
+"""
+CP1404 Practical 5
+Program that allows the hexadecimal codes to be looked up based on user input.
+"""
 COLOUR_TO_HEX_CODE = {"absolute zero": "#0048ba", "acid green": "#b0bf1a", "aliceblue": "#f0f8ff",
                       "alizarin crimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00",
                       "amethyst": "#9966cc", "antiquewhite": "#9966cc", "antiquewhite1": "#ffefdb",
