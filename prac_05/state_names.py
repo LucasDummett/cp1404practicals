7d0909d6776bc58921da003c496719d743f6be32
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-14 09:28:09 +1000

    Add state_names program in prac 5

---
+++ prac_05/state_names.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
+is_valid_input = False
+while not is_valid_input:
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+        is_valid_input = True
+    except KeyError:
+        print("Invalid short state")
+        state_code = input("Enter short state: ").upper()
+
+
+for code in CODE_TO_NAME:
+    print(f"{code:3} is {CODE_TO_NAME[code]}")
