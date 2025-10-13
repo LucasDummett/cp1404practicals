840a6d86cc5134173dcd0432ce945efc5b4bf6af
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-14 09:48:13 +1000

    Add hex_colours program in prac 5

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+COLOUR_TO_HEX_CODE = {"absolute zero": "#0048ba", "acid green": "#b0bf1a", "aliceblue": "#f0f8ff",
+                      "alizarin crimson": "#e32636", "amaranth": "#e52b50", "amber": "#ffbf00",
+                      "amethyst": "#9966cc", "antiquewhite": "#9966cc", "antiquewhite1": "#ffefdb",
+                      "antiquewhite2": "#eedfcc"}
+
+colour = input("Enter colour: ").lower()
+while colour != "":
+    try:
+        print(colour, "is", COLOUR_TO_HEX_CODE[colour])
+        colour = input("Enter colour: ").lower()
+    except KeyError:
+        print("Invalid colour")
+        colour = input("Enter colour: ").lower()
