b8587efc9d710085b3a2522d2e2cc16aa80f6e7e
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-10-28 13:26:55 +1000

    Add __str__ method to Car class in car.py and update used_cars.py program to use it

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,16 +9,15 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "Car")
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
 
-
-    limo = Car(100)
+    limo = Car(100, "Limo")
     limo.add_fuel(20)
-    print(limo.fuel)
+    print(f"Limo has fuel: {limo.fuel}")
     limo.drive(115)
-
+    print(limo)
+    print(my_car)
 
 main()
