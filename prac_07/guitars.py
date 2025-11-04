22f370e80eb4ea7e8c44fbaf5e2d8fe5d68afa9b
Lucas Dummett <lucas.dummett@my.jcu.edu.au>
2025-11-04 13:13:51 +1000

    Add user guitar input code to guitars.py program in Prac_07

--- prac_07/guitars.py
+++ prac_07/guitars.py
@@ -5,12 +5,13 @@ import os
 sys.path.append(os.path.expanduser(r"E:"))
 from guitar import Guitar
 
+FILENAME = "guitars.csv"
 
 def main():
     """Read file of programming language details, save as objects, display."""
     guitars = []
     # Open the file for reading
-    in_file = open('guitars.csv', 'r')
+    in_file = open(FILENAME, 'r')
     # 'Consume' the first line (header) - we don't need its contents
     in_file.readline()
     for line in in_file:
@@ -23,6 +24,18 @@ def main():
     # Close the file as soon as we've finished reading it
     in_file.close()
 
+    print("My guitars!")
+    for guitar in guitars:
+        print(guitar)
+    name = input("Name: ")
+    while name != "":
+        year = str(input("Year: "))
+        cost = float(input("Cost: "))
+        add_guitar = Guitar(name, year, cost)
+        guitars.append(add_guitar)
+        print(add_guitar, "added.")
+        name = input("Name: ")
+
     # Loop through and display all languages (using their str method)
     for guitar in guitars:
         print(guitar)
@@ -32,4 +45,9 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+
+    with open(FILENAME, "w", encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            out_file.write(f"{guitar.name}, {guitar.year}, {guitar.cost}\n")
+
 main()
