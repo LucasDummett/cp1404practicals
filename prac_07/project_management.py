b0cc16c68df02b3ec08b8549a2d9f6c636851ead
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-11-05 13:53:27 +1000

    Add project exercise programs with low functionality

---
+++ prac_07/project_management.py
@@ -0,0 +1,34 @@
+from project import Project
+
+FILENAME = "projects.txt"
+TEST_FILE = "test.txt"
+
+
+def main():
+    projects = load_projects(FILENAME)
+    save_projects(TEST_FILE, projects)
+
+
+def load_projects(filename):
+    """Load projects from file projects.txt."""
+    projects = []
+    with open(filename, "r", encoding="utf-8") as infile:
+        infile.readline()
+        for line in infile:
+            parts = line.strip().split("\t")
+            projects.append(Project(name=parts[0], start_date=parts[1], priority=parts[2], cost_estimate=parts[3],
+                                    completion_percentage=parts[4]))
+        projects.sort()
+    return projects
+
+
+def save_projects(filename, projects):
+    """Save projects to file."""
+    with open(filename, "w", encoding="utf-8") as outfile:
+        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=outfile)
+        for project in projects:
+            print(f"{project.name} \t{project.start_date} \t{project.priority} \t{project.cost_estimate} "
+                  f"\t{project.completion_percentage}", file=outfile)
+
+
+main()
