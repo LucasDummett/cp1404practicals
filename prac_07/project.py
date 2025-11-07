f26028d54ab5e8b4e364c32a0b2c72ee880e26af
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-11-07 18:15:35 +1000

    Update class constructor for project.py class in prac_07

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,4 +1,5 @@
 """
+project.py
 Estimate: 50 mins
 Actual: 30 mins
 """
@@ -10,14 +11,15 @@ class Project:
     """Class representing a project."""
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
-        """Initialize a project instances."""
+        """Initialise a project instance."""
         self.name = name
-        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
-        self.priority = int(priority)
-        self.cost_estimate = float(cost_estimate)
-        self.completion_percentage = int(completion_percentage)
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
 
     def __lt__(self, other):
+        """Enable sorting by comparing objects based on the priority field. """
         return self.priority < other.priority
 
     def __str__(self):
@@ -26,8 +28,10 @@ class Project:
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%")
 
     def is_after(self, given_date):
+        """Determine if project start date is equal to or after given date."""
         return self.start_date >= given_date
 
     def detail_to_row(self) -> str:
+        """Return a formatted string with tabbed project fields."""
         return (f"{self.name}\t{self.start_date.strftime('%d/%m/%Y')}\t"
                 f"{self.priority}\t{self.cost_estimate}\t{self.completion_percentage}")
