e94e65d8f1b25cf1df684be0e7ea1a6eb174ebda
Easy-Company5 <lucas.dummett@my.jcu.edu.au>
2025-11-16 14:44:55 +1000

    Update convert_miles_km program and kivy file in prac_08

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,12 +1,13 @@
 """
 Estimate: 60 mins
-Actual:  mins
+Actual: 75 mins
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
+MILES_TO_KM_RATIO = 1.609344
 
 
 class MileToKmApp(App):
@@ -14,10 +15,29 @@ class MileToKmApp(App):
     output_km = StringProperty()
 
     def build(self):
-        """Build the Kivy app from the kv file."""
+        """Build the Kivy app from the .kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_distance_conversion(self):
+        """Calculate kilometers from mile input (user input or increment)."""
+        number_of_miles = self.get_valid_input()
+        number_of_kilometers = number_of_miles * MILES_TO_KM_RATIO
+        self.root.ids.output_label.text = str(number_of_kilometers)
+
+    def handle_increment(self, increment):
+        """Increment the string input with button pressed and update the label value."""
+        number_of_miles = self.get_valid_input() + increment
+        self.root.ids.input_miles.text = str(number_of_miles)
+        self.handle_distance_conversion()
+
+    def get_valid_input(self):
+        """Convert input to float, returning 0 if invalid input."""
+        try:
+            return float(self.root.ids.input_miles.text)
+        except ValueError:
+            return 0
 
 
 MileToKmApp().run()
